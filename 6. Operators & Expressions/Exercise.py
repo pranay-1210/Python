e4eb62b2933d40108eb643f0142b393bbd74bc93
Pranay <praveenpranay07@gmail.com>
2026-02-06 16:48:49 +0530

    Learnt about the first operator i.e, Arithmetic operator along with a coding exercise of finding the area of a rectangle.

---
+++ 6. Operators & Expressions/Exercise.py
@@ -0,0 +1,6 @@
+# Program to find the area of a rectangle
+
+length = 10
+breadth = 5
+area = length * breadth
+print('Area is: ',area)
---
+++ 6. Operators & Expressions/operators.py
@@ -0,0 +1,11 @@
+# Arithmiteic Operators:
+
+a = 14
+b = 4
+print(a + b) # 18
+print(a - b) # 10
+print(a * b) # 56
+print(a / b) # 3.5 Float Division
+print(a // b) # 3 Floor Division
+print(a % b) # 2 
+print(a ** b) # 1764
