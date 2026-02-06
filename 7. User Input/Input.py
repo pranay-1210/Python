73c52d3d7ca87975425e50c8660fdccce03cef25
Pranay <praveenpranay07@gmail.com>
2026-02-06 17:03:14 +0530

    Learnt about how to take input from the user and then showing the result,also implemented it using various practice exercises.

---
+++ 7. User Input/Input.py
@@ -0,0 +1,25 @@
+l = int(input("Enter the length of the rectangle: "))
+b = int(input("Enter the breadth of the rectangle: "))
+area = l * b
+print("Area is: ",area)
+
+# We can also use print("Area is: ",l*b)
+
+# Program to find the area of a triangle
+
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+area = 0.5 * base * height
+print("Area is: ",area)
+
+# Program to find the area of a circle
+
+radius = int(input("Enter the radius of the circle: "))
+area = 3.14 * radius * radius
+print("Area is: ",area)
+
+# Program to find the area of a square
+
+side = int(input("Enter the side of the square: "))
+area = side * side
+print("Area is: ",area)
